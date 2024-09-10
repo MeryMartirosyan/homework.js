ae77fe427c4e25f78943ba67e5d33ef67c1d9647
Mery <mmeri5359@gmail.com>
2024-09-09 21:41:14 -0700

    homework.js

--- homework.js
+++ homework.js
@@ -1,1 +1,8 @@
-let a = 9;
+let number = 9; // Ex.1 => if the number is odd print odd, if it's even print even
+if(number % 2 === 0) {
+   console.log('even');
+} else if(number % 2 !== 0) {
+   console.log('odd');
+}
+
+
