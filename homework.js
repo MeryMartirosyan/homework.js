b723143b612509b2eb8f98d125da7c09a73c4aeb
Mery <mmeri5359@gmail.com>
2024-09-11 20:56:06 -0700

    homework.js

--- homework.js
+++ homework.js
@@ -28,3 +28,26 @@ function averageOfNumbers(num1, num2, num3, num4, num5) {  // Ex.4 calculate the
       const average = (num1 + num2 + num3 + num4 + num5) / 5;
             return average
 }
+
+
+
+function multipleOfNum(num) { // Ex.5 => if multiple of 3, 5, 7, output the appropriate message
+   if(num % 3 === 0 && num % 5 === 0 && num % 7 === 0) {
+      return num + ' is a multiple of 3, 5 and 7.'
+  } else if(num % 3 !== 0 && num % 5 !== 0 && num % 7 !== 0) {
+      return num + ' is not a multiple of 3, 5 or 7.'
+  } else if(num % 3 === 0 && num % 5 === 0) {
+      return num + ' is a multiple of 3 and 5.'
+  } else if(num % 5 === 0 && num % 7 === 0) {
+      return num + ' is a multiple of 5 and 7.'
+  } else if(num % 3 === 0 && num % 7 === 0) {
+      return num + ' is a multiple of 3 and 7.'
+  } else if(num % 3 === 0) {
+      return num + ' is a multiple of 3.'
+  } else if(num % 5 === 0) {
+      return num + ' is a multiple of 5.'
+  } else if(num % 7 === 0) {
+      return num + ' is a multiple of 7.'
+  }
+}
+
