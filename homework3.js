2c759ef0582496eda58ddac581e47e52ad30f3ba
Mery Martirosyan <mmeri5359@gmail.com>
2024-09-26 10:58:15 -0700

    homework3.js

--- homework3.js
+++ homework3.js
@@ -1,1 +1,8 @@
+//Ex.1(for loop) Given a number. Print the sum of digits.
+let sumOfDigits = 0;
+for(let number = +prompt(); number !== 0; number -= Math.floor(number / 10)) {
+    sumOfDigits += number % 10;
+}
+console.log(sumOfDigits);
 
+//Ex.1 (while loop)
