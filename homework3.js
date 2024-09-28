4dc580459b8a946a67ecab7463c5cfa518ab5990
Mery Martirosyan <mmeri5359@gmail.com>
2024-09-27 19:11:55 -0700

    homework3.js

--- homework3.js
+++ homework3.js
@@ -1,11 +1,11 @@
-//Ex.1(for loop) Given a number. Print the sum of digits.
+//Ex.1 => (for loop) Given a number. Print the sum of digits.
 let sumOfDigits = 0;
 for(let number = +prompt(); number !== 0; number -= Math.floor(number / 10)) {
     sumOfDigits += number % 10;
 }
 console.log(sumOfDigits);
 
-//Ex.1 (while loop)
+//Ex.1 => (while loop)
 let num = +prompt('enter a number');
 if(num < 0) {
     num = - num;
@@ -18,7 +18,7 @@ while(num !== 0) {
 }
 console.log(sumOfDigits);
 
-//(for loop)Given a number. Print the multiplication of digits.let num = +prompt();
+//Ex.2 => (for loop)Given a number. Print the multiplication of digits.let num = +prompt();
 if(num < 0) {
     num = - num;
 }
@@ -41,6 +41,31 @@ while(num > 0) {
     num = Math.floor(num / 10);
 }
 console.log(multip);
+
+/* Ex.3 => (for loop) Insert a number. Calculate product and sum of the digits of the number. If product is
+divisible by the sum, print the quotient, otherwise print the remainder. */
+let number = +prompt('Enter a number.');
+if(number === 0) {
+    console.log('Cannot be calculated');
+} else {
+    let product = 1, sum = 0;
+    if(number < 0) {
+        number = - number;
+    }
+    for(let a = 0; number > 0; number = Math.floor(number / 10)) {
+        let digit = number % 10;
+        product *= digit;
+        sum += digit;
+    }
+    if(product % sum === 0) {
+        console.log('Quotient is ' + product / sum);
+    } else {
+        console.log('Remainder is ' + product % sum);
+    }
+}
+
+//(while loop)
+
 
 
 
