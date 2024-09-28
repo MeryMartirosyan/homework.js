201d95b7a6b5a319ebc829703766da116200c38c
Mery Martirosyan <mmeri5359@gmail.com>
2024-09-27 18:41:07 -0700

    homework3.js

--- homework3.js
+++ homework3.js
@@ -6,3 +6,14 @@ for(let number = +prompt(); number !== 0; number -= Math.floor(number / 10)) {
 console.log(sumOfDigits);
 
 //Ex.1 (while loop)
+let num = +prompt('enter a number');
+if(num < 0) {
+    num = - num;
+}
+let sumOfDigits = 0;
+while(num !== 0) {
+    let digit = num % 10;
+    sumOfDigits += digit;
+    num = (num - digit) / 10;
+}
+console.log(sumOfDigits);
