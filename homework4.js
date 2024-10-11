d8951157402dc00ead4771248c8105459ef80bed
Mery Martirosyan <mmeri5359@gmail.com>
2024-10-10 19:47:32 -0700

    homework4.js

---
+++ homework4.js
@@ -0,0 +1,17 @@
+// Given a positive integer. Bring the last digit of the number to the beginning. Print the new
+// number. If the last digit of the inserted number is 0, number remains the same. 
+
+function bringLastDigitToFront(number) {
+    if(number === 0) {
+        console.log(0);
+        return 0
+    } else {
+        let lastDigit = number % 10;
+        number = Math.floor(number / 10);
+        number = String(number);
+        let result = '';
+        result += lastDigit + number;
+        console.log(+result);
+        return +result
+    }
+}
