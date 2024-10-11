58a0fd38430bcdbaf85d7e7d96ccc9b146b96e49
Mery Martirosyan <mmeri5359@gmail.com>
2024-10-10 19:48:14 -0700

    homework4.js

--- homework4.js
+++ homework4.js
@@ -1,6 +1,5 @@
-// Given a positive integer. Bring the last digit of the number to the beginning. Print the new
-// number. If the last digit of the inserted number is 0, number remains the same. 
-
+/* Ex.1 ==>>  Given a positive integer. Bring the last digit of the number to the beginning. Print the new
+ number. If the last digit of the inserted number is 0, number remains the same. */
 function bringLastDigitToFront(number) {
     if(number === 0) {
         console.log(0);
