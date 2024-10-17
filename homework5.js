0365bf34f8b218d30a181692fbcc30f2598da5f9
Mery Martirosyan <mmeri5359@gmail.com>
2024-10-17 10:46:18 -0700

    homework5.js

--- homework5.js
+++ homework5.js
@@ -1,1 +1,15 @@
+/* Ex.1 ==>> Write a function that accepts a string(a sentence) as a parameter and finds the longest
+word within the string․ If there are several words which are the longest ones, print the
+last word (words can be separated by space). */ 
 
+
+
+/* Ex.2 ==>> Write a function to find longest substring in a given a string without repeating characters
+except space character.
+If there are several, return the last one. Consider that all letters are lowercase. */
+
+
+
+/* Ex.3 ==>> Write a function to compute a new string from the given one by moving the first char to
+come after the next two chars, so &quot;abc&quot; yields &quot;bca&quot;. Repeat this process for each
+subsequent group of 3 chars. Ignore any group of fewer than 3 chars at the end. */
