540c8b20e219b5a0766e6ae4a06ed2e2fbb56859
Mery Martirosyan <mmeri5359@gmail.com>
2024-10-17 20:34:16 -0700

    homework5.js

--- homework5.js
+++ homework5.js
@@ -24,6 +24,18 @@ If there are several, return the last one. Consider that all letters are lowerca
 
 
 
+
 /* Ex.3 ==>> Write a function to compute a new string from the given one by moving the first char to
 come after the next two chars, so &quot;abc&quot; yields &quot;bca&quot;. Repeat this process for each
 subsequent group of 3 chars. Ignore any group of fewer than 3 chars at the end. */
+function changeThree(str) {      // DOES NOT RETURN THE LAST ELEMENT OF THE ODD STRING
+    let subStr = '', result = '';
+    for(let i = 0; i <= str.length; i+=3) {
+        subStr += str[i + 1] + str[i + 2] + str[i];
+        if(subStr.length <= 3) {
+                result += subStr;
+        }
+        subStr = '';
+    }
+    return result
+}
