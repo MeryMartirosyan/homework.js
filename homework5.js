9ff76629276201b19f09b9b05f51c70d91141c27
Mery Martirosyan <mmeri5359@gmail.com>
2024-10-17 19:16:12 -0700

    homework5.js

--- homework5.js
+++ homework5.js
@@ -1,8 +1,22 @@
 /* Ex.1 ==>> Write a function that accepts a string(a sentence) as a parameter and finds the longest
 word within the string․ If there are several words which are the longest ones, print the
 last word (words can be separated by space). */ 
-
-
+function theLongestWord(sentence) {
+    let word = '', word2 = '', result = '';
+    for(let i = 0; i < sentence.length; i++) {
+        if(sentence[i] !== ' ') {
+            word += sentence[i];
+        }
+        if(sentence[i] === ' ') {
+                if(word.length > word2.length) {
+                    result = word;
+                }
+            word2 = word;
+            word = '';
+        }
+    }
+    return result
+}
 
 /* Ex.2 ==>> Write a function to find longest substring in a given a string without repeating characters
 except space character.
