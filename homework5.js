8e96ed212be2b73b6c5b9f52ee97e30b55adcbb1
Mery Martirosyan <mmeri5359@gmail.com>
2024-10-24 08:24:45 -0700

    Update

--- homework5.js
+++ homework5.js
@@ -42,12 +42,18 @@ function longestNotRepeated(str) {          // not completed fully
 /* Ex.3 ==>> Write a function to compute a new string from the given one by moving the first char to
 come after the next two chars, so &quot;abc&quot; yields &quot;bca&quot;. Repeat this process for each
 subsequent group of 3 chars. Ignore any group of fewer than 3 chars at the end. */
-function changeThree(str) {      // not completed yet
+function changeThree(str) {  
+    debugger;
     let subStr = '', result = '';
-    for(let i = 0; i <= str.length; i+=3) {
-        subStr += str[i + 1] + str[i + 2] + str[i];
-        if(subStr.length <= 3) {
-                result += subStr;
+    for(let i = 0; i < str.length; i+=3) {
+        if(str.length - i === 2) {
+            result += str[i] + str[i + 1];
+        }
+     subStr += str[i + 1] + str[i + 2] + str[i];      
+        if(subStr !== 'NaN' || subStr !== 'undefined') {
+            if(subStr.length <= 3) {
+               result += subStr;
+            }
         }
         subStr = '';
     }
