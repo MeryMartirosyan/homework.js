96fae3e4c71a1524036c72baa5c8dec5a6e11786
Mery Martirosyan <mmeri5359@gmail.com>
2024-10-31 20:28:58 -0700

    homework6.js

--- homework6.js
+++ homework6.js
@@ -1,1 +1,16 @@
+/* Given an object. Invert it (keys become values and values
+become keys) */   
 
+function reverse(obj) { 
+   debugger;
+    let newObj = {};
+    for(let key in obj) {
+        if(newObj[key] !== undefined) {
+            newObj[key] = obj[key];
+        }
+        if(obj[key] !== undefined) {
+            newObj[obj[key]] = key;
+        }
+    }
+    return newObj
+}
