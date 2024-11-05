95cb1193b5978a8622bdcc50f54a18095a2d9a00
Mery Martirosyan <mmeri5359@gmail.com>
2024-11-05 09:57:30 -0800

    homework7.js

--- homework7.js
+++ homework7.js
@@ -1,1 +1,17 @@
+/* Ex.1  Given an array of strings and numbers. Print the number of integers and the number of
+strings in the array. */
+function fn(arr) {
+    let countNum = 0, countStr = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if(typeof arr[i] === 'number') {
+            countNum++;
+        }
+        if(typeof arr[i] === 'string') {
+            countStr++;
+        }
+    }
+    const result = `Numbers: ${countNum}, Strings: ${countStr}`;
+    return result
+}
+
 
